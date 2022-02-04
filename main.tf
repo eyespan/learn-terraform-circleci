47ef6b83e450399055c94010bcbdcf3f26f37208
yohannes@theserverlabs.com <yohannes@theserverlabs.com>
2022-02-04 18:09:00 +0000

    Add remote backend and variable definitions

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "3.44.0"
     }
   }
+  backend "s3" {
+    bucket = "circle-ci-backend-20220204180238283700000001"
+    key    = "terraform/webapp/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_version = "> 0.14"
 }
 
